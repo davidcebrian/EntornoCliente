114cc48527cf1e0de9fbeb3a0ddb43de07e814f8
David <davidcebriancortes0@gmail.com>
2020-09-22 11:18:05 +0200

    Ejercicios PruebaInicial completados

--- PruebaInicial/ej2DavidCC.js
+++ PruebaInicial/ej2DavidCC.js
@@ -3,52 +3,88 @@ let numero = document.getElementsByName('num')
 let tabla = document.getElementById('tabla')
 let dnis = document.getElementsByName('id')
 let flag = false
+let total = 3;
 
 const nuevoEmp = () =>{
     let nombre = prompt('Introduzca nombre:')
     let dni = prompt('Introduzca DNI')
     let apellidos = prompt('Introduzca apellidos')
-
-    let newRow = document.createElement('tr')
-
-    let newpos = document.createElement('td')
-    let newdni = document.createElement('td')
-    let newnom = document.createElement('td')
-    let newape = document.createElement('td')
-    newpos.setAttribute('name', 'pos')
-    newdni.setAttribute('name', 'id')
-
-    let postext = document.createTextNode(parseInt(numero[numero.length-1].textContent) +1)
-    let dnitext = document.createTextNode(dni)
-    let nomtext = document.createTextNode(nombre)
-    let apetext = document.createTextNode(apellidos)
-    
-   for(let i = 0; i<dnis.length; i++){
+    let flag = false;
+   for(let i = 0; i<dnis.length && !flag; i++){
        if(dnis[i].textContent == dni){
            alert('YA EXISTE')
-           
-       }else {
-    newpos.appendChild(postext)
-    newdni.appendChild(dnitext)
-    newnom.appendChild(nomtext)
-    newape.appendChild(apetext)
+           flag = true;
+       }
+    }
+    if(!flag){
+        let newRow = document.createElement('tr')
+    
+        let newpos = document.createElement('td')
+        let newdni = document.createElement('td')
+        let newnom = document.createElement('td')
+        let newape = document.createElement('td')
+        newpos.setAttribute('name', 'pos')
+        newdni.setAttribute('name', 'id')
+        newnom.setAttribute('name', 'nom')
+        newape.setAttribute('name', 'ape')
+    
+        let postext = document.createTextNode(parseInt(numero[numero.length-1].textContent) +1)
+        let dnitext = document.createTextNode(dni)
+        let nomtext = document.createTextNode(nombre)
+        let apetext = document.createTextNode(apellidos)
 
-    newRow.appendChild(newpos)
-    newRow.appendChild(newdni)
-    newRow.appendChild(newnom)
-    newRow.appendChild(newape)
+        newpos.appendChild(postext)
+        newdni.appendChild(dnitext)
+        newnom.appendChild(nomtext)
+        newape.appendChild(apetext)
 
-    tabla.appendChild(newRow)
+        newRow.appendChild(newpos)
+        newRow.appendChild(newdni)
+        newRow.appendChild(newnom)
+        newRow.appendChild(newape)
+
+        tabla.appendChild(newRow)
+        total++
+        document.getElementById('total').textContent = 'Numero total de empleados: ' + total
    }
 }
-}
+
    const borrarEmp = () => {
        let dniborrar = prompt('Introduzca DNi de quien quiera borrar.')
-       for(let i = 0; i< dnis.length ; i++){
+       let flag = false;
+       for(let i = 0; i< dnis.length && !flag ; i++){
         if(dnis[i].textContent == dniborrar){
             tabla.deleteRow(i + 1)
+            flag=true;
+            total--
+            document.getElementById('total').textContent = 'Numero total de empleados: ' + total
+
         }
     }
+        if(flag == false){
+            alert('no existe empleado')
+        }
+
 
 }
 
+
+    const modificar = () => {
+        
+        let dnimod = prompt('Introduzca DNI de quien quiere mod:')
+        let flag = false;
+        for(let i = 0; i< dnis.length ; i++){
+            if(dnis[i].textContent == dnimod){
+                let nombre = prompt('que nombre quiere ponerle:')
+                let ape = prompt ('que apellido quiere ponerle:')
+                document.getElementsByName('nom')[i].textContent = nombre
+                document.getElementsByName('ape')[i].textContent = ape
+                flag = true;
+            }
+        }
+        if(!flag){
+            alert('no existe')
+        }
+
+    
+    }
