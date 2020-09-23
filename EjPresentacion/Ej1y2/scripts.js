c8960a99c70a98ede55f3c3b4698aadcd58a0a61
David <davidcebriancortes0@gmail.com>
2020-09-23 09:12:13 +0200

    Hasta el ej3

--- EjPresentacion/Ej1y2/scripts.js
+++ EjPresentacion/Ej1y2/scripts.js
@@ -1,5 +1,7 @@
+//EJ1y2
 let caja = document.getElementById('box')
 let input = document.getElementById('tex')
+
 
 caja.addEventListener('mousedown', () => {
     console.log('Has pulsado la caja.')
@@ -10,10 +12,26 @@ caja.addEventListener('mouseup', () => {
 })
 
 input.addEventListener('keydown', (e) => {
-    
     console.log('Has pulsado la tecla ' + e.key)
 })
 
 input.addEventListener('keyup', (e) => {
     console.log('Has soltado la tecla tecla ' + e.key)
+})
+
+//EJ3
+let input2 = document.getElementById('text')
+let btn = document.getElementById('btn')
+
+btn.addEventListener('click', () =>{
+    input2.addEventListener('keyup', (e) =>{
+        console.log(e.key)
+    })
+})
+
+//EJ4
+let gallery = document.getElementById('gallery')
+
+gallery.addEventListener('click', (e) => {
+    console.log(e.target.textContent)
 })
