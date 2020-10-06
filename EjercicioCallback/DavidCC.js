89f352e69d086f2215a46e36ec78d39a408de1e5
David <davidcebriancortes0@gmail.com>
2020-10-06 11:24:01 +0200

    cb

---
+++ EjercicioCallback/DavidCC.js
@@ -0,0 +1,20 @@
+let usuarios = [{
+        name: david,
+        username: rizo,
+        id: 1
+    },
+    {
+        name: yi,
+        username: chen,
+        id: 2
+    },
+    {
+        name: david,
+        username: gomez,
+        id: 3
+    }
+]
+
+const getUser = (id, cb) =>{
+    
+}
