5f57e03c5bb5a8845bbaa244feb3329a89b6d4cf
David <davidcebriancortes0@gmail.com>
2020-10-14 08:34:59 +0200

    Ejercicios AJAX y Fetch terminados

--- EjercicioAJAXJSON/script.js
+++ EjercicioAJAXJSON/script.js
@@ -22,3 +22,20 @@ btn.addEventListener('click', () => {
 
     xhr.send()
 })
+
+const btnf = document.getElementById("btnf")
+btnf.addEventListener('click', () => {
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(res => res.ok ? Promise.resolve(res) : Promise.reject(res))
+    .then(res => res.json())
+    .then(res => {
+        for(user of res){
+            let fila = document.createElement('li')
+            fila.textContent = `${user.id} - ${user.name}`
+            lista.appendChild(fila)
+        }
+        lista.innerHTML
+    })
+})
+
+
