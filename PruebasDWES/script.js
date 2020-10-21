9f19d04c1f38e1558ec465d455b43e432e84e719
David <davidcebriancortes0@gmail.com>
2020-10-21 10:03:46 +0200

    Ejercicio FETCHS BBDD completos

--- PruebasDWES/script.js
+++ PruebasDWES/script.js
@@ -1,3 +1,6 @@
+/**Cogemos todos los botones y los imputs para utilizarlos 
+ * mas tarde
+ */
 let button = document.getElementById('btn')
 let buttonGet = document.getElementById('btnG')
 
@@ -6,7 +9,9 @@ let surname = document.getElementById('surname')
 let userName = document.getElementById('nickname')
 let mobileNumber = document.getElementById('mobile')
 let mail = document.getElementById('email')
-
+/**Cuando clickamos obtenemos los datos de los imputs en otras variables
+ * y creamos un objeto con los datos del usuario
+ */
 button.addEventListener('click', (e) => {
     e.preventDefault()
     let name2 = name.value
@@ -22,6 +27,9 @@ button.addEventListener('click', (e) => {
         mobileNumber: mobileNumber2,
         mail: mail2
     }
+    /**Hacemos la petición POST a nuestro endpoint y le pasamos el objet
+     * usuario que hemos creado como JSON en el body
+     */
     fetch('http://localhost:8080/user', {
             method: 'POST',
             body: JSON.stringify(newUser),
@@ -35,7 +43,9 @@ button.addEventListener('click', (e) => {
             console.log(res)
         })
 })
-
+/**Hacemos la petición GET a nuestro endpoint y los datos recibidos 
+los mostramos en el formulario.
+*/
 buttonGet.addEventListener('click', (e) => {
     e.preventDefault()
     let id = document.getElementById('id').value
@@ -51,3 +61,31 @@ buttonGet.addEventListener('click', (e) => {
         }
         )
 })
+
+/**Cogemos el elemento lista para añadirle mas tarde 
+ * los elementos a mostrar y el boton para listar los elementos.
+ */
+let lista = document.getElementById('list')
+let listbtn = document.getElementById('btnlista')
+
+/**Hacemos una petición get de todos los usuarios a nuestro endpoint
+ * al clickar en el boton LISTAR
+ */
+listbtn.addEventListener('click', (e) => {
+    let ulvacio = document.createElement('ul')
+    ulvacio.setAttribute('id', 'list')
+    lista.replaceWith(ulvacio)
+    fetch('http://localhost:8080/user')
+    .then(res => res.ok ? Promise.resolve(res) : Promise.reject(res))
+    .then(res => res.json())
+    .then(res => {
+        for(user of res){
+            let elem = document.createElement('li')
+            let text = document.createTextNode(`ID: ${user.id}` + ' - '+`${user.name}`)
+            elem.appendChild(text)
+            ulvacio.appendChild(elem)
+        }
+    })
+})
+
+
