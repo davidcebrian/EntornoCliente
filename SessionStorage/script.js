4f4b74bc68861f7cda5aff65ab30c18e4fc92554
David <davidcebriancortes0@gmail.com>
2020-10-28 09:21:17 +0100

    session storage terminado

--- SessionStorage/script.js
+++ SessionStorage/script.js
@@ -1,13 +1,19 @@
 let btnSave = document.getElementById('Guardar');
 let btnSelect = document.getElementById('Select');
 
+let input = document.getElementById('key');
+let inputval = document.getElementById('value');
+
+/* botón guardar, guarda la clave y valor en 
+    session storage */
 btnSave.addEventListener('click', (e) => {
     e.preventDefault();
     sessionStorage.setItem(form.key.value, form.value.value);
 })
 
+/* botón buscar, busca el valor de la key introducida
+    y la muestra en el campo value. */
 btnSelect.addEventListener('click', (e) => {
     e.preventDefault();
-    let obj = sessionStorage.getItem(form.key.value)
-    form.value.value = obj.value;
+    inputval.value = sessionStorage.getItem(input.value)
 })
